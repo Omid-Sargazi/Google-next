b80a0860c54cb1f5e4fadeac674a3d37c0bc4e4d
Omid <om.sargazi@gmail.com>
2024-02-19 20:42:52 +0330

    create search page for web and image

---
+++ src/app/search/image/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ImageSearchPage = () => {
+  return <div className=" bg-gray-500">ImageSearch</div>;
+};
+
+export default ImageSearchPage;
---
+++ src/app/search/web/page.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function WebSearchPage() {
+  return <div className=" bg-gray-600">WebSearchPage</div>;
+}
